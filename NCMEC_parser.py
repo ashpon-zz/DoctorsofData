64427cbe840c8602354cbe51196f91e911116bb7
Jamie Jin <jamiejin91@gmail.com>
2017-09-21 15:52:22 -0400

    Add files via upload

--- NCMEC_parser.py
+++ NCMEC_parser.py
@@ -1,61 +1,59 @@
-
-# coding: utf-8
-
-# In[36]:
-
 import json
 import requests
 import pandas as pd
 
 session = requests.Session()
-
-df = pd.DataFrame(columns = ['name','age','missing_date','missing_city','missing_county','missing_state',])
-
-
-# In[37]:
+df = pd.DataFrame(columns = ['name','age','sex','ethnicity','height','weight','eye_color','hair_color','NCMEC_case#','case_type','missing_date','missing_city','missing_county','missing_state',])
 
 state = input("Which State to gather data from?(Abbreviations only) ")
 query_url_temp = "http://www.missingkids.com/missingkids/servlet/JSONDataServlet?action=publicSearch&searchLang=en_US&search=new&subjToSearch=child&missState=" + state + "&missCountry=US"
-response_temp = session.get(query_url_temp)
-#print("{} {}".format(response.status_code, response.reason))
-
-
-# In[39]:
-
-response_temp_json = json.loads(response_temp.text)
-#print(json.dumps(response_temp_json, sort_keys=True, indent=4, separators=(',', ': ')))
-
-
-# In[43]:
+try:
+    response_temp = session.get(query_url_temp)
+    response_temp_json = json.loads(response_temp.text)
+except:
+    print("api timeout T.T")
+    quit()
 
 for i in range(int(response_temp_json['totalPages'])):
     query_url_loop = "http://www.missingkids.com/missingkids/servlet/JSONDataServlet?action=publicSearch&searchLang=en_US&goToPage={}".format(i+1)
     response_loop = session.get(query_url_loop)
     response_loop_json = json.loads(response_loop.text)
-    #print("starting loop " + str(i+1))
-    #print(query_url_loop)
+    
     for ii in range(len(response_loop_json['persons'])):
-        person_name = response_loop_json['persons'][ii]['firstName'] + " " + response_loop_json['persons'][ii]['lastName']
-        if response_loop_json['persons'][ii]['middleName'] != "":
-            person_name.replace("", " {} ".format(response_loop_json['persons'][ii]['middleName']))
+        # detailed response
+        try:
+            detailed_response = session.get("http://www.missingkids.com/missingkids/servlet/JSONDataServlet?action=childDetail&caseNum={}&orgPrefix={}".format(response_loop_json['persons'][ii]["caseNumber"], response_loop_json['persons'][ii]["orgPrefix"]))
+            detailed_response_json = json.loads(detailed_response.text)
+            person_info = detailed_response_json['childBean']
+        except:
+            print("api timeout T.T")
+            quit()
         
-        index_num = (len(response_loop_json['persons']) * i) + ii
+        # first + middle + last name
+        person_name = person_info['firstName'] + " " + person_info['lastName']
+        if person_info['middleName'] != "":
+            person_name.replace("", " {} ".format(person_info['middleName']))
+        
+        # index number of df
+        index_num = (len(response_temp_json['persons']) * i) + ii
         try:
             df.set_value(index_num,'name',person_name)
-            df.set_value(index_num,'age',response_loop_json['persons'][ii]['age'])
-            df.set_value(index_num,'missing_date',response_loop_json['persons'][ii]['missingDate'])
-            df.set_value(index_num,'missing_city',response_loop_json['persons'][ii]['missingCity'])
-            df.set_value(index_num,'missing_county',response_loop_json['persons'][ii]['missingCounty'])
-            df.set_value(index_num,'missing_state',response_loop_json['persons'][ii]['missingState'])
+            df.set_value(index_num,'age',person_info['age'])
+            df.set_value(index_num,'sex',person_info['sex'])
+            df.set_value(index_num,'ethnicity',peson_info['race'])
+            df.set_value(index_num,'height',person_info['height'])
+            df.set_value(index_num,'weight',person_info['weight'])
+            df.set_value(index_num,'eye_color',person_info['eyeColor'])
+            df.set_value(index_num,'hair_color',person_info['hairColor'])
+            df.set_value(index_num,'NCMEC_case#',person_info['caseNumber'])
+            df.set_value(index_num,'case_type',person_info['caseType'])
+            df.set_value(index_num,'missing_date',person_info['missingDate'])
+            df.set_value(index_num,'missing_city',person_info['missingCity'])
+            df.set_value(index_num,'missing_county',person_info['missingCounty'])
+            df.set_value(index_num,'missing_state',person_info['missingState'])
         except:
-            print('error T.T')
+            print('error T.T - person {}, page {}'.format(i+1,ii+1))
             continue
-        #print(person_name)
+        print("person {}, page {} complete".format(i+1,ii+1))
             
 df.to_csv("NCMEC_{}.csv".format(state), encoding="utf-8", index=False)
-
-
-# In[ ]:
-
-
-
